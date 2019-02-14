0cb5527a14142addfb5b3b42c85f4b1f7dc355eb
odc.heliangcheng <hlc19890519@126.com>
2019-02-14 15:00:09 +0800

    猿空间 apespace_recruit招聘模块微服务

--- apespace_recruit/src/main/java/com/hlc/apespace/recruit/RecruitApplication.java
+++ apespace_recruit/src/main/java/com/hlc/apespace/recruit/RecruitApplication.java
@@ -3,6 +3,7 @@ package com.hlc.apespace.recruit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
     *@ClassName RecruitApplication
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 **/
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class RecruitApplication {
 
     public static void main(String[] args) {
